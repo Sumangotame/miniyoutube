ad0a29b5d05b7e77423188b07d3f512f633d89b7
Sumangotame <iamsuperstarsuman@gmail.com>
2024-02-14 12:34:09 +0545

    search button alignment fixed

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -14,11 +14,11 @@ function SearchBar({ onSearch }) {
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
                 placeholder="Search videos..."
-                className="border border-gray-300 rounded-md px-3 py-2 mr-2 outline-zinc-900 my-2"
+                className="border border-gray-300 rounded-md px-3 py-2 mr-2 outline-zinc-900 mb-6"
             />
             <button
                 onClick={handleSearch}
-                className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 mb-6"
+                className="bg-blue-500 text-white px-3 py-2 rounded-md hover:bg-blue-600 mb-6"
             >
                 Search
             </button>
