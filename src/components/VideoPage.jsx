170cf15996fb541efca968704bfa3dd93543eabe
Sumangotame <iamsuperstarsuman@gmail.com>
2024-02-14 12:24:32 +0545

    fixed grid gap

--- src/components/VideoPage.jsx
+++ src/components/VideoPage.jsx
@@ -27,7 +27,7 @@ function VideoPage() {
             {loading ? (
                 <p>Search Video First...</p>
             ) : (
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5">
                     {videos.map((video) => (
                         <VideoThumbnail key={video.id} video={video} />
                     ))}
